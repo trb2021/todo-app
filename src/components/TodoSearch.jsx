016bdd2f06aa52990d88de807836520808b0b4f1
trb2021 <mazzito2015@gmail.com>
2022-03-03 13:55:04 -0500

    Maquetado y CSS de los componentes de la aplicación

---
+++ src/components/TodoSearch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/TodoSearch.css';
+
+function TodoSearch () {
+    return (
+        <>
+            <input className="TodoSearch" placeholder="Cebolla" />
+        </>
+    );
+}
+
+export default TodoSearch;

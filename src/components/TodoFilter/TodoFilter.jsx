6ce21c6b42944f8de621fe4547a9301e502c560b
trb2021 <mazzito2015@gmail.com>
2022-04-04 14:33:59 -0400

    Terminada funcionalidad de filtrado, falta solo refinar estilos visuales

--- src/components/TodoFilter/TodoFilter.jsx
+++ src/components/TodoFilter/TodoFilter.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TodoContext } from "../../context/TodoContext";
 import './TodoFilter.css';
 
 function TodoFilter() {
-    return(
+  
+  const {setFilterTodo} = useContext(TodoContext);
+
+  const onClickHandler = (element) => {
+    setFilterTodo(element.target.id);
+  }
+  
+  return(
         <>
           <div className="filter">
-            <button id="all" className="on">Todos</button>
-            <button id="active">Activos</button>
-            <button id="completed">Completados</button>
+            <button id="all" onClick={onClickHandler}>Todos</button>
+            <button id="active" onClick={onClickHandler}>Activos</button>
+            <button id="completed" onClick={onClickHandler}>Completados</button>
           </div>
           <div className="corner">
             <button id="clear-completed">Borrar completados</button>

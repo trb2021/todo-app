2e53d44cfd1c7395fa7897cb7fd63f3f00337a58
trb2021 <mazzito2015@gmail.com>
2022-04-05 08:56:37 -0400

    Ajuste de estilos visuales a los filtros de TODOs

--- src/components/TodoFilter/TodoFilter.jsx
+++ src/components/TodoFilter/TodoFilter.jsx
@@ -8,12 +8,14 @@ function TodoFilter() {
 
   const onClickHandler = (element) => {
     setFilterTodo(element.target.id);
+    document.querySelector(".on").classList.remove("on");
+    document.querySelector("#"+element.target.id).classList.add("on");    
   }
   
   return(
         <>
           <div className="filter">
-            <button id="all" onClick={onClickHandler}>Todos</button>
+            <button id="all" className="on" onClick={onClickHandler}>Todos</button>
             <button id="active" onClick={onClickHandler}>Activos</button>
             <button id="completed" onClick={onClickHandler}>Completados</button>
           </div>
